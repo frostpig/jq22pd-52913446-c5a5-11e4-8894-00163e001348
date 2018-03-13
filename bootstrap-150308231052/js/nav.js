f0c5de832d1b076438ca7b4657d0a1d9376a4731
frostpig <1397233369@qq.com>
2018-03-13 17:41:22 +0800

    Initial commit

---
+++ bootstrap-150308231052/js/nav.js
@@ -0,0 +1,76 @@
+jQuery(document).ready(function(){
+
+    var qcloud={};
+
+    $('[_t_nav]').hover(function(){
+
+        var _nav = $(this).attr('_t_nav');
+
+        clearTimeout( qcloud[ _nav + '_timer' ] );
+
+        qcloud[ _nav + '_timer' ] = setTimeout(function(){
+
+            $('[_t_nav]').each(function(){
+
+                $(this)[ _nav == $(this).attr('_t_nav') ? 'addClass':'removeClass' ]('nav-up-selected');
+
+            });
+
+            $('#'+_nav).stop(true,true).slideDown(200);
+
+        }, 150);
+
+    },function(){
+
+        var _nav = $(this).attr('_t_nav');
+
+        clearTimeout( qcloud[ _nav + '_timer' ] );
+
+        qcloud[ _nav + '_timer' ] = setTimeout(function(){
+
+            $('[_t_nav]').removeClass('nav-up-selected');
+
+            $('#'+_nav).stop(true,true).slideUp(200);
+
+        }, 150);
+
+    });
+
+});
+
+$(function(){
+    var thisTime;
+    $('.nav-ul li').mouseleave(function(even){
+        thisTime	=	setTimeout(thisMouseOut,1000);
+    })
+
+    $('.nav-ul li').mouseenter(function(){
+        clearTimeout(thisTime);
+        var thisUB	=	$('.nav-ul li').index($(this));
+        if($.trim($('.nav-slide-o').eq(thisUB).html()) != "")
+        {
+            $('.nav-slide').addClass('hover');
+            $('.nav-slide-o').hide();
+            $('.nav-slide-o').eq(thisUB).show();
+        }
+        else{
+            $('.nav-slide').removeClass('hover');
+        }
+
+    })
+
+    function thisMouseOut(){
+        $('.nav-slide').removeClass('hover');
+    }
+
+    $('.nav-slide').mouseenter(function(){
+        clearTimeout(thisTime);
+        $('.nav-slide').addClass('hover');
+    })
+    $('.nav-slide').mouseleave(function(){
+        $('.nav-slide').removeClass('hover');
+    })
+
+
+
+})
